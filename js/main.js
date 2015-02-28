b22b3689e8d4eac0c74767ac0129cd8b4ec83b91
Jahsie Ault <jahsieault@gmail.com>
2015-02-27 23:59:36 -0800

    Navbar 90% done. Need to fix resizing glitch, minor fixes to icon sizing

--- js/main.js
+++ js/main.js
@@ -19,11 +19,26 @@
   		$("#waterP").addClass("hidden");	
   	});
 
+
+
+  	// var width = $( window ).width();
+  	// var widthCap = 768px;
+  	// if (width > widthCap) {
+  	// 	$(".menu").removeClass("hidden");
+  	// }
+
+
   	$(".nav-icons").on('click', function() {
 	$(".menu").slideToggle("hidden");
- //  	$("#exerciseP").addClass("hidden");
-	// $("#waterP").addClass("hidden");	
 	});
+
+
+  	var section = $(window);
+	var width = section.width();
+	var nav = $('.menu');
+	if (width > 768) {
+    	nav.removeClass('hidden');
+	} 
 
 
   });
